6b0b5f9b35e49d97d54e5dee6c83addc7d48b387
erincunningham7 <>
2023-11-29 22:29:44 +0000

    register models and fix spelling issue in category model

--- products/models.py
+++ products/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
-
 
     def __str__(self):
         return self.name
